5be4661831fc11d8233891dbe6a6d3c43916931a
frm2 <fernandorangelm96@gmail.com>
2022-09-27 16:12:07 -0300

    montei o ambiente

---
+++ src/desafioJusta/Desafio1.java
@@ -0,0 +1,12 @@
+package desafioJusta;
+
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStreamReader;
+
+public class Desafio1 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
